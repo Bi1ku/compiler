4710c820e9f3a77c438da498d8f835c9c996db8f
Bi1ku <2008owenshi@gmail.com>
2024-08-11 14:12:43 -0400

    lexer progress

---
+++ include/utils.h
@@ -0,0 +1,6 @@
+#ifndef LEXER
+#define LEXER
+
+int in_array(char *arr[], char str[], int length);
+
+#endif
--- src/lexer.c
+++ src/lexer.c
@@ -1,13 +1,45 @@
+#include "../include/utils.h"
 #include <stdio.h>
+#include <string.h>
+
+typedef enum {
+  keyword,
+  number,
+  string,
+  operator,
+  newline,
+} type;
+
+typedef struct {
+  type type;
+  char value[100];
+} token;
 
 int lexer() {
   FILE *file = fopen("../tests/print.bi", "r");
 
-  char output[100];
+  char *keywords[] = {"print"};
+  int length = sizeof(keywords) / sizeof(keywords[0]);
+
+  char output[1000];
+  token tokens[1000];
 
   while (fgets(output, 100, file)) {
-    printf("%s", output);
+    char *word = strtok(output, " ");
+
+    while (word) {
+      if (in_array(keywords, word, length)) {
+        tokens[0].type = keyword;
+        strcpy(tokens[0].value, word);
+      }
+
+      printf("%s\n", word);
+      word = strtok(NULL, " ");
+    }
   }
+
+  printf("%d\n", tokens[0].type); // 0 (keyword)
+  printf("%s\n", tokens[0].value);
 
   fclose(file);
 
